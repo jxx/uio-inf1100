25e18ff9759905a8bd6f3609ad73fb8c97d3c9ab
jxx <jon.martin.gulliksen@gmail.com>
2013-08-28 09:55:30 +0200

    Add print line

--- kick.py
+++ kick.py
@@ -3,7 +3,7 @@ from math import pi
 drag_coeff = 0.2 # Drag coefficient, dimesionless
 rho = 1.2 # Density of air, kg/m^3
 ball_radius = .11 # m
-A =  pi * ball_radius**2 # Area of ball, m^2
+ball_area =  pi * ball_radius**2 # Area of ball, m^2
 ball_mass = 0.43 # kg
 g = 9.81 # Gravitational constant, m/s^2
 gravity_force = ball_mass*g # N
@@ -11,3 +11,8 @@ gravity_force = ball_mass*g # N
 V_hard = 120 * 5/18 # Hard kick velocity of ball, km/h converted to m/s
 V_soft = 10 * 5/18 # Soft kick velocity of ball, km/h converted to m/s
 
+
+
+drag_force = 0.5 * drag_coeff * rho * ball_area * V_hard
+
+print drag_force
