2f411b457d55707326540b45e066ad3846d04824
jxx <jon.martin.gulliksen@gmail.com>
2013-08-28 09:33:52 +0200

    Changed gaussian formula

--- Gaussian_function1.py
+++ Gaussian_function1.py
@@ -4,7 +4,11 @@ m = 0.
 s = 2.
 x = 1.
 
-gaussian = ( 1 / sqrt( 2 * pi ) * s  ) * exp( ( -1./2.  * ( (x-m) / s) ** 2 ) ) 
+gaussian = ( 1 / ( sqrt( 2 * pi ) * s )  ) * exp( ( -1./2.  * ( (x-m) / s) ** 2 ) ) 
 
 print gaussian
 
+"""
+~/uio-inf1100 $ python Gaussian_function1.py 
+0.176032663382
+"""
