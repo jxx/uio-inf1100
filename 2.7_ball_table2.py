83e0d1fa426690647c70a06b2bdb4e1843f5b963
jxx <jon.martin.gulliksen@gmail.com>
2013-09-04 10:47:33 +0200

    2.7 updated with while loop, tolerance level

--- 2.7_ball_table2.py
+++ 2.7_ball_table2.py
@@ -9,22 +9,21 @@ t_values = []
 y_values = []
 
 # Set v0 = 1, g = 9.81, and n = 11
-v0 = 1.
+v0 = 1
 g = 9.81
 n = 11
+tolerance = 1E-14 # fudge since float operator is not precise
 
-for i in range(n+1):
-    
-    # Use n uniformly spaced t values throughout the interval [0, 2v0/g] 
-    t_step = 2 * v0 / g
-    t = (i + 1) * t_step
-    y_t = v0 * t - 0.5 * g * t ** 2
+# compute t values
+dt = (2 * v0/g - 0)/(n-1) # spacing of t values
+t = 0
 
-    # so that the t and y values are stored in two lists t and y.    
+while t <= 2 * v0 / g + tolerance:
     t_values.append(t)
-    y_values.append(y_t)
+    t = t + dt
 
-    #print '%.3f %.3f' % (t_values[i], y_values[i])
+# Using list comprehension for y_values:
+y_values = [v0 * t - 0.5 * g * t ** 2 for t in t_values]
 
 # Thereafter, transverse the lists with a for loop and write out
 # a nicely formatted table of t and y values 
@@ -33,17 +32,16 @@ for t_value, y_value in zip(t_values, y_values):
     print '%8.3f %8.3f' % (t_value, y_value)
 
 """
-~/uio-inf1100$ python 2.7_ball_table2.py 
-   0.204    0.000
-   0.408   -0.408
-   0.612   -1.223
-   0.815   -2.446
-   1.019   -4.077
-   1.223   -6.116
-   1.427   -8.563
-   1.631  -11.417
-   1.835  -14.679
-   2.039  -18.349
-   2.243  -22.426
-   2.446  -26.911
+uio-inf1100$ python 2.7_ball_table2.py 
+   0.000    0.000
+   0.020    0.018
+   0.041    0.033
+   0.061    0.043
+   0.082    0.049
+   0.102    0.051
+   0.122    0.049
+   0.143    0.043
+   0.163    0.033
+   0.183    0.018
+   0.204   -0.000
 """
