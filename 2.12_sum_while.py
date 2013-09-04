4bac4f9ac231ced404fad7491b2f568880d705b0
jxx <jon.martin.gulliksen@gmail.com>
2013-09-04 10:50:26 +0200

    2.12 Do while errors corrected

--- 2.12_sum_while.py
+++ 2.12_sum_while.py
@@ -4,4 +4,11 @@
 #    s += 1/k
 # print s
 
- 
+s = 0.; k = 01.; M = 100 
+# needs s counter to avoid infinite loop
+# k, s should be floats to calculate correctly
+# k needs to start at 0 for loop to execute properly
+while k < M:
+    k += 1 # added counter.
+    s += 1/k 
+print s 
