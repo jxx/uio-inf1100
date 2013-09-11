4caf9fa270704a5c6bbc76731d23d5158d45a051
jxx@asus <jon.martin.gulliksen@gmail.com>
2013-09-11 10:44:59 +0200

    Runtime example for 3.03

--- 3.03_roots_quadratic.py
+++ 3.03_roots_quadratic.py
@@ -11,3 +11,11 @@ for item in test1:
 test2 = roots(1, 4, 1)
 for item in test2:
     print item, type(item)
+
+"""
+$ python 3.03_roots_quadratic.py 
+(-1+9.94987437107j) <type 'numpy.complex128'>
+(-1-9.94987437107j) <type 'numpy.complex128'>
+-0.267949192431 <type 'numpy.float64'>
+-3.73205080757 <type 'numpy.float64'>
+"""
