f5f32a24565cf1d544e604d55d72fabed9b4d0b0
jxx@asus <jon.martin.gulliksen@gmail.com>
2013-11-28 14:00:54 +0100

    Add class notes

---
+++ 8.08_sum9_4_dice.py
@@ -0,0 +1,10 @@
+"""Exercise 8.8. Decide if a dice game is fair.
+Somebody suggests the following game. You pay 1 euro and are
+allowed to throw four dice. If the sum of the eyes on the dice is less than
+9, you win r euros, otherwise you lose your investment. Should you play
+this game when r = 10? Answer the question by making a program
+that simulates the game. Read r and the number of experiments N
+from the command line Name of program file: sum9_4dice.py."""
+
+
+⋄
